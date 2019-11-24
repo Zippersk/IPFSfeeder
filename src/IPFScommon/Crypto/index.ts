bd3727b9aab1c4146907a0e8c23002be8e9dfcc7
Matus <mucka@netsearch.cz>
2019-11-24 18:10:02 +0100

    remove

--- src/IPFScommon/Crypto/index.ts
+++
@@ -1,3 +0,0 @@
-exports.Address = require('./address')
-exports.Block = require('./block')
-exports.Transaction = require('./transaction')
--- src/IPFScommon/IPLD/jsonFormat/index.ts
+++
@@ -1,4 +0,0 @@
-exports.resolver = require('./resolver.js')
-exports.util = require('./util.js')
-exports.codec = exports.util.codec
-exports.defaultHashAlg = exports.util.defaultHashAlg
