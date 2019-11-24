38c0404da3298c9a5baf95c949688821b3eac783
Matus <mucka@netsearch.cz>
2019-11-24 23:58:05 +0100

    add p2p ipfs

--- src/BlockBook/blockbookApi.ts
+++ src/BlockBook/blockbookApi.ts
@@ -1,4 +1,4 @@
-export abstract class BlockBookApi {
+export default abstract class BlockBookApi {
     public static GetTransactions() {
         return transactions
     }
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,38 @@
-import { IPFSconnector } from "./Common/IPFS/IPFSConnector"
-import { BlockBookApi } from './BlockBook/blockbookApi'
+import IPFSconnector from "./Common/IPFS/IPFSConnector"
+import BlockBookApi from './BlockBook/blockbookApi'
+import ipfsDefaultConfig from './Common/IPFS/ipfsDefaultConfig'
+import os from 'os';
+import DAG from './Common/IPFS/DAG';
+import logger from './Common/logger';
 
 async function runAsync() {
+    let config = ipfsDefaultConfig
+    config.repo = os.homedir() + '/.ipfsFeeder'
+    config.config = {
+        Addresses: {
+            Swarm: [
+                '/ip4/0.0.0.0/tcp/14012',
+                '/ip4/127.0.0.1/tcp/14013/ws'
+            ],
+            API: '/ip4/127.0.0.1/tcp/5012',
+            Gateway: '/ip4/127.0.0.1/tcp/9191'
+        }
+    }
+
+    IPFSconnector.setConfig(config)
     var ipfsInstance = await IPFSconnector.getInstanceAsync()
     try {
         var txs = BlockBookApi.GetTransactions()
         for (const tx of txs) {
-            console.log((await ipfsInstance.dagPutAsync(tx)).toString())
+            logger.info((await DAG.PutAsync(tx)).toString())
+            logger.info(await DAG.GetAsync(tx.hash, null))
         }
     } catch (error) {
         ipfsInstance.shutDown()
         throw error
     }
+
+
 }
 
 runAsync()
@@ -21,4 +42,6 @@ runAsync()
     .catch((e) => {
         throw e;
     })
+
+
 
